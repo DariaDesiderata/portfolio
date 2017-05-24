e59e17b9a541a0ba769dddad480e48f3c943c430
DariaDesiderata <daria.calwell@gmail.com>
2017-05-23 22:18:26 -0600

    add projects section

--- public/main.js
+++ public/main.js
@@ -1,16 +1,38 @@
 $().ready(() => {
-  $('#scroll').on('click', function(e) {
+  $('.scroll').on('click', function(e) {
   	e.preventDefault()
   	$('html, body').animate({
       scrollTop: $('#skills').offset().top
-    }, 500)
+    }, 700, 'swing')
   })
   var options = [
-    {selector: '#skills', offset: 500, callback: function(el) {
-        Materialize.fadeInImage($(el));
+    {
+      selector: '#skills', offset: 500, callback: function(el) {
+        Materialize.fadeInImage($(el))
       }
-    }  
+    },
+    {
+      selector: '#projectLeft1', offset: 0, callback: function(el) {
+        $(el).addClass('animated fadeInUp')
+      }
+    },
+    {
+      selector: '#projectRight1', offset: 150, callback: function(el) {
+        $(el).addClass('animated fadeInUp')
+      }
+    },
+    {
+      selector: '#projectLeft2', offset: 0, callback: function(el) {
+        $(el).addClass('animated fadeInUp')
+      }
+    },
+    {
+      selector: '#projectRight2', offset: 0, callback: function(el) {
+        $(el).addClass('animated fadeInUp')
+      }
+    }
   ]
+
   $('.parallax').parallax();
   Materialize.scrollFire(options);
 })
